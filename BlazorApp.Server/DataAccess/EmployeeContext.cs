fa2c31c49d75542678f251d1a302818349799e73
hDoubleE <Trevor.Heehs@stmartin.edu>
2018-12-11 09:38:30 -0800

    initial commit

---
+++ BlazorApp.Server/DataAccess/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Server.DataAccess
+{
+    public class EmployeeContext : DbContext
+    {
+        public virtual DbSet<BlazorApp.Shared.Models.Employee> tblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=tcp:ServerName,1433;Initial Catalog=TDatabase;Persist Security Info=False;User ID=UserName;Password=Password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ct1-group-project
-Group Project
+### Group Project
+Simple Blazor Web App written entirely in C# and Razor(html).
+
+Create, Read, Update, Delete operations.
